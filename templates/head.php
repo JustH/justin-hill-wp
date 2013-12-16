ec943efcff1f0210453881c110e7a02e7ae9fb39
JustH <contact@justin-hill.com>
2013-12-16 00:28:35 -0800

    updated primary brand color, change sidebar layout

--- templates/head.php
+++ templates/head.php
@@ -4,6 +4,8 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
+  <!-- add font awesome from CDN -->
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <?php wp_head(); ?>
